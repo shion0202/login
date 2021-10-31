126b7e605a5b58e524047623bb40a8003b706068
urarik <a01076684995@gmail.com>
2021-11-01 00:49:00 +0900

    aa

--- app/src/main/java/com/example/loginmine/utils/ActivityNavigation.kt
+++ app/src/main/java/com/example/loginmine/utils/ActivityNavigation.kt
@@ -1,4 +1,7 @@
 package com.example.loginmine.utils
 
+import android.content.Intent
+
 interface ActivityNavigation {
+    fun startActivityForResult(intent: Intent?, requestCode: Int)
 }
--- app/src/main/java/com/example/loginmine/view/LoginActivity.kt
+++ app/src/main/java/com/example/loginmine/view/LoginActivity.kt
@@ -1,11 +1,15 @@
 package com.example.loginmine.view
 
-import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -14,38 +18,57 @@ import com.example.loginmine.R
 import com.example.loginmine.UserInfo
 import com.example.loginmine.databinding.ActivityLoginBinding
 import com.example.loginmine.datatype.User
-import com.example.loginmine.utils.CoroutinesAsyncTask
-import com.example.loginmine.utils.NaverLoginHandler
+import com.example.loginmine.utils.ActivityNavigation
 import com.example.loginmine.viewmodel.LoginViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.nhn.android.naverlogin.OAuthLogin
-import com.nhn.android.naverlogin.OAuthLoginHandler
-import org.json.JSONObject
-import java.net.URLDecoder
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
-class LoginActivity : AppCompatActivity() {
+
+class LoginActivity : AppCompatActivity(), ActivityNavigation {
 
     lateinit var loginBinding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
     private val user = User()
-    private val loginViewmodel: LoginViewModel by lazy {
+    private val loginViewModel: LoginViewModel by lazy {
         ViewModelProvider(this as ViewModelStoreOwner).get(LoginViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         loginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-        loginBinding.viewmodel = loginViewmodel
+        loginBinding.viewmodel = loginViewModel
         loginBinding.user = user
         initObservers()
         initNaverLogin()
+        initGoogleLogin()
     }
 
     private fun initObservers() {
-        loginViewmodel.isLogin.observe(this, Observer {
+        val transact = {
+            Toast.makeText(this, "welcome, ${UserInfo.userName}", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        loginViewModel.isLogin.observe(this, Observer {
             if (it) {
-                Toast.makeText(this, "welcome, ${UserInfo.userName}", Toast.LENGTH_LONG).show()
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+                transact()
             }
+        })
+        loginViewModel.cheatEvent.observe(this, {
+            val editText = EditText(this)
+
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("메모")
+            builder.setView(editText)
+            builder.setPositiveButton("입력", DialogInterface.OnClickListener{
+                dialog, which ->
+                Toast.makeText(applicationContext, editText.text, Toast.LENGTH_SHORT).show()
+            })
+            builder.show()
         })
     }
 
@@ -57,10 +80,56 @@ class LoginActivity : AppCompatActivity() {
             "mL_2dNGHcu",
             "LoginMine"
         )
-        val mOAuthLoginHandler = NaverLoginHandler(this, mOAuthLoginModule, loginViewmodel::callback)
+        val mOAuthLoginHandler =
+            LoginViewModel.NaverLoginHandler(this, mOAuthLoginModule, loginViewModel::naverLogInCallback)
         loginBinding.buttonNaverLogin.setOAuthLoginHandler(mOAuthLoginHandler)
-        loginBinding.buttonNaverLogin.setBgResourceId(R.drawable.btn_g)
+        loginBinding.buttonNaverLogin.setBgResourceId(R.drawable.btn_ag)
     }
 
+    private fun initGoogleLogin() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("861154748604-j0o0t3hh3epjvlslecr3uq6pucuorrst.apps.googleusercontent.com")
+                .requestEmail()
+                .build()
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = FirebaseAuth.getInstance()
+        loginViewModel.setGoogleClient(googleSignInClient)
+        loginViewModel.googleLoginEvent.setEventReceiver(this, this)
+        loginBinding.buttonGoogleLogin.setOnClickListener {
+            loginViewModel.onGoogleLogin()
+        }
+        val textView = loginBinding.buttonGoogleLogin.getChildAt(0) as TextView
+        textView.text = getString(R.string.google_login)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == loginViewModel.RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d("tag", "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Log.d("tag", "Google sign in failed", e)
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("tag", "signInWithCredential:success")
+                        val user = auth.currentUser
+                        loginViewModel.updateUI(user!!.email!!)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("tag", "signInWithCredential:failure", task.exception)
+                    }
+                }
+    }
 
 }
